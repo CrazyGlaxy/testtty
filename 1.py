be226fecd8f10367b007fa427b22f9c033dd7241
Crazy Glaxy <Crazy Glaxy>
2025-07-19 10:22:30 +0600

    shift logic implemented

--- 1.py
+++ 1.py
@@ -1,50 +1,118 @@
 import random
 import math
 
-def generate():
-    arr = [i for i in range(9)]
-    shuffled = arr[:]
-    random.shuffle(shuffled)
-    return shuffled
-arr = generate()
-print(arr)  # Output: A shuffled list of numbers from 0 to 8
+class Board:
+    def __init__(self):
+        self.generate()
 
-def parity_checker(arr):
-    arr[:] = [1,2,3,4,5,6,7,8,0]
-    count = 0
-    for i in range(len(arr)):
-        for j in range(i):
-            if arr[i] and j<i and arr[j]> arr[i]: count+=1
-    return count
+    def generate(self)->None:
+        self.arr = [i for i in range(9)]
+        shuffled = self.arr[:]
+        random.shuffle(shuffled)
+        self.arr = shuffled
+        self.arr[:] = [1,2,3,4,5,6,7,8,0]
+
+    def parity_checker(self):
+        count = 0
+        for i in range(len(self.arr)):
+            for j in range(i):
+                if self.arr[i] and self.arr[j]> self.arr[i]: count+=1
+        return count
+            
+    def print_board(self):
+        for i in range(3):
+            for j in range(3):
+                print(self.arr[j+i*3], end=" ")
+            print()
+
+    def row(self):
+        return (self.arr.index(0)//3)
         
-def print_board(arr):
-    for i in range(3):
-        for j in range(3):
-            print(arr[j+i*3], end=" ")
+    def col(self):
+        index = self.arr.index(0)
+        return (index - self.row()*3)
+
+    def shift(self, direction: str)->bool:
+        row = self.row()
+        col = self.col()
+
+        if direction == "left" and col != 0:
+            target = row*3 + col - 1
+        elif direction == "right" and col != 2:
+            target = row*3 + col + 1
+        elif direction == "up" and row != 0:
+            target = row*3 + col - 3
+        elif direction == "down" and row != 2:
+            target = row*3 + col + 3
+        else:
+            return False
+        
+        self.arr[row*3 + col] = self.arr[target]
+        self.arr[target] = 0
+        
+        return True
+    
+    def shift_left(self)->bool:
+        return self.shift("left")
+        
+    def shift_right(self)->bool:
+        return self.shift("right")
+        
+    def shift_up(self):
+        return self.shift("up")
+    
+    def shift_down(self):
+        return self.shift("down")
+    
+
+# board = Board()
+# board.print_board()
+# print()
+# print(board.arr)
+# print("Checking Parity:", board.parity_checker(), "\n")
+# print("row: ",board.row())
+# print("col: ", board.col())
+
+class Test:
+    def __init__(self):
+        board = Board()
+        board.print_board()
         print()
+        print(board.arr)
+        print("Checking Parity:", board.parity_checker(), "\n")
+        print("row: ",board.row())
+        print("col: ", board.col())
+        print("shift left:", board.shift_left())
+        board.print_board()
+        print("shift left:", board.shift_left())
+        board.print_board()
+        print("shift left:", board.shift_left())
+        board.print_board()
+        print()
+        
+        # shift right
+        print("shift right:", board.shift_right())
+        board.print_board()
+        print("shift right:", board.shift_right())
+        board.print_board()
+        print("shift right:", board.shift_right())
+        board.print_board()
 
-# def shift_left():
-#     row = round(arr.index(0)/3)
-#     col
+        # shift up
+        print("shift up:", board.shift_up())
+        board.print_board()
+        print("shift up:", board.shift_up())
+        board.print_board()
+        print("shift up:", board.shift_up())
+        board.print_board()
 
-    
-
-print(parity_checker(arr))
-print(arr)
-print_board(arr)
-
-
-print("hhhhhhhhhhhhhhh")
-# for i in range(len(arr)):
-#     row = math.floor(arr.index(i)/3)
-#     print(row)
-    
-print(arr[0]/3)
-print("hhhhhhhhhhhhhhh")
-
-# col finding replace i by index hew hwhw wh wwh
-for i in range(len(arr)):
-    row = math.floor(i/3)
-    col = i-(row*3)
-    print(col)
-
+        # shift down
+        
+        print("shift down:", board.shift_down())
+        board.print_board()
+        print("shift down:", board.shift_down())
+        board.print_board()
+        print("shift down:", board.shift_down())
+        board.print_board()
+        
+Test()
